3d12479f61331792385fc1e9f920ec3bbf7d5112
minkova-diyana <minkova.di@gmail.com>
2025-01-13 15:14:03 +0200

    try: test

--- health_assist/settings.py
+++ health_assist/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '16.170.146.192']
+ALLOWED_HOSTS = []
 
 # Application definition
 MY_APPS = [
